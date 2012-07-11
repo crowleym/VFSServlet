b68c47e2ede74c60e1697b8494718fece8179aee
Mark Crowley <crowley_mark@yahoo.com>
2012-07-11 20:01:34 +0100

    Added comments

--- README.md
+++ README.md
@@ -2,3 +2,11 @@ VFSServlet
 ==========
 
 A simple servlet that allows you to return a file within a file using Apache Commons VFS
+
+example call:
+
+http://localhost:8080/files/path/to/somefile.zip!/path/to/somefile.jar!/path/to/index.txt
+
+Will return the file /path/to/index.txt within somefile.jar
+where somefile.jar is the file /path/to/somefile.jar within somefile.zip
+where somefile.zip is the file /path/to/somefile.zip relative to the filesystem directory defined in the servlet initParam FileStorePath
--- src/com/adstream/servlets/VFSServlet.java
+++ src/com/adstream/servlets/VFSServlet.java
@@ -1,12 +1,5 @@
 package com.adstream.servlets;
 
-/**
- * Created with IntelliJ IDEA.
- * User: Mark.Crowley
- * Date: 11/07/12
- * Time: 15:01
- */
-
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.vfs2.*;
@@ -14,9 +7,19 @@ import org.apache.commons.vfs2.*;
 import javax.servlet.http.*;
 import java.io.*;
 
+/**
+ * A simple servlet that allows you to return a file within a file using Apache Commons VFS
+ *
+ * example call:
+ * http://localhost:8080/files/path/to/somefile.zip!/path/to/somefile.jar!/path/to/index.txt
+ *
+ * Will return the file /path/to/index.txt within somefile.jar
+ * where somefile.jar is the file /path/to/somefile.jar within somefile.zip
+ * where somefile.zip is the file /path/to/somefile.zip relative to the filesystem directory defined in the servlet initParam FileStorePath
+ */
 public class VFSServlet extends HttpServlet {
 
-    private String fileStorePath = null;
+    private String fileStorePath = null; //root directory on filesystem
 
     public void init() {
         fileStorePath = getServletConfig().getInitParameter("FileStorePath");
@@ -33,12 +36,14 @@ public class VFSServlet extends HttpServlet {
 
             StringBuilder buff = new StringBuilder();
 
+            //Determine what file extensions if any we need to drill in to
             int pos = 0, i;
             while((i = vfsFilePath.indexOf("!/", pos)) != -1) {
-                buff.append(FilenameUtils.getExtension(vfsFilePath.substring(pos, i))).append(':');
+                buff.insert(0, FilenameUtils.getExtension(vfsFilePath.substring(pos, i)) + ':');
                 pos = i+1;
             }
 
+            //build up the required path for Apache Commons VFS
             buff.append("file://").append(fileStorePath).append(vfsFilePath);
 
             fsManager = VFS.getManager();
